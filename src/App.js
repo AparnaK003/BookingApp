250ce4cb86bc5425fe8fb14fd582791775532635
AparnaK003 <114304807+AparnaK003@users.noreply.github.com>
2023-05-03 17:02:50 +0530

    Frontend code for Booking app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import List from "./pages/list/List.jsx";
+import Hotel from "./pages/hotel/Hotel.jsx";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/hotels" element={<List/>}/>
+      <Route path="/hotels/:id" element={<Hotel/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
